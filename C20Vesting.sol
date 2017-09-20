0aae888fae5e469c85b767986aed179239ca186a
Matthew Finlayson <matthew.james.finlayson@gmail.com>
2017-09-20 19:29:30 +0200

    convert to COP paradigm

--- C20Vesting.sol
+++ C20Vesting.sol
@@ -1,97 +1,126 @@
-pragma solidity 0.4.11;
 
-import './Token.sol';
-import './SafeMath.sol';
+  pragma solidity 0.4.11;
 
-/**
- * @title C20Vesting
- * @dev C20Vesting is a token holder contract that allows the specified beneficiary
- * to claim stored tokens after 6 month intervals
-*/
- contract C20Vesting is SafeMath {
+  import './Token.sol';
+  import './SafeMath.sol';
 
-   address public beneficiary;
+  /**
+   * @title C20Vesting
+   * @dev C20Vesting is a token holder contract that allows the specified beneficiary
+   * to claim stored tokens after 6 month intervals
+  */
 
-   uint256 public fundingEndBlock;
+   contract C20Vesting is SafeMath {
 
-   bool private initClaim = false; // state tracking variables
+      address public beneficiary;
+      uint256 public fundingEndBlock;
 
-   uint256 public firstRelease; // vesting times
-   bool private firstDone = false;
-   uint256 public secondRelease;
-   bool private secondDone = false;
-   uint256 public thirdRelease;
-   bool private thirdDone = false;
-   uint256 public fourthRelease;
+      bool private initClaim = false; // state tracking variables
 
-   Token public ERC20Token; // ERC20 basic token contract to hold
+      uint256 public firstRelease; // vesting times
+      bool private firstDone = false;
+      uint256 public secondRelease;
+      bool private secondDone = false;
+      uint256 public thirdRelease;
+      bool private thirdDone = false;
+      uint256 public fourthRelease;
 
-   function C20Vesting(address _token, uint256 fundingEndBlockInput) {
-     beneficiary = msg.sender;
-     fundingEndBlock = fundingEndBlockInput;
-     ERC20Token = Token(_token);
+      Token public ERC20Token; // ERC20 basic token contract to hold
+
+      enum Stages {
+          initClaim,
+          firstRelease,
+          secondRelease,
+          thirdRelease,
+          fourthRelease
+      }
+
+      Stages public stage = Stages.initClaim;
+
+      modifier atStage(Stages _stage) {
+          if(stage == _stage) _;
+      }
+
+      function C20Vesting(address _token, uint256 fundingEndBlockInput) {
+          require(_token != address(0));
+          beneficiary = msg.sender;
+          fundingEndBlock = fundingEndBlockInput;
+          ERC20Token = Token(_token);
+      }
+
+      function updateToken(address tokenAddressInput) external {
+          require(tokenAddressInput != address(0));
+          require(msg.sender == beneficiary);
+          ERC20Token = Token(tokenAddressInput);
+      }
+
+      function changeBeneficiary(address newBeneficiary) external {
+          require(newBeneficiary != address(0));
+          require(msg.sender == beneficiary);
+          beneficiary = newBeneficiary;
+      }
+
+      function checkBalance() constant returns (uint256 tokenBalance) {
+          return ERC20Token.balanceOf(this);
+      }
+
+      // in total 13% of C20 tokens will be sent to this contract
+      // EXPENSE ALLOCATION: 5.5%       | TEAM ALLOCATION: 7.5% (vest over 2 years)
+      //   2.5% - Marketing             | initalPayment: 1.5%
+      //   1% - Security                | firstRelease: 1.5%
+      //   1% - legal                   | secondRelease: 1.5%
+      //   0.5% - Advisors              | thirdRelease: 1.5%
+      //   0.5% - Boutnty               | fourthRelease: 1.5%
+      // initial claim is tot expenses + initial team payment
+      // initial claim is thus (5.5 + 1.5)/13 = 53.846153846% of C20 tokens sent here
+      // each other release (for team) is 11.538461538% of tokens sent here
+
+      function claim() external {
+          require(msg.sender == beneficiary);
+          require(block.number > fundingEndBlock);
+          uint256 balance = ERC20Token.balanceOf(this);
+          // in reverse order so stages changes don't carry within one claim
+          fourth_release(balance);
+          third_release(balance);
+          second_release(balance);
+          first_release(balance);
+          init_claim(balance);
+      }
+
+      function nextStage() private {
+          stage = Stages(uint256(stage) + 1);
+      }
+
+      function init_claim(uint256 balance) private atStage(Stages.initClaim) {
+          firstRelease = now + 26 weeks; // assign 4 claiming times
+          secondRelease = firstRelease + 26 weeks;
+          thirdRelease = secondRelease + 26 weeks;
+          fourthRelease = thirdRelease + 26 weeks;
+          uint256 amountToTransfer = safeMul(balance, 53846153846) / 100000000000;
+          ERC20Token.transfer(beneficiary, amountToTransfer); // now 46.153846154% tokens left
+          nextStage();
+      }
+      function first_release(uint256 balance) private atStage(Stages.firstRelease) {
+          require(now > firstRelease);
+          uint256 amountToTransfer = balance / 4;
+          ERC20Token.transfer(beneficiary, amountToTransfer); // send 25 % of team releases
+          nextStage();
+      }
+      function second_release(uint256 balance) private atStage(Stages.secondRelease) {
+          require(now > secondRelease);
+          uint256 amountToTransfer = balance / 3;
+          ERC20Token.transfer(beneficiary, amountToTransfer); // send 25 % of team releases
+          nextStage();
+      }
+      function third_release(uint256 balance) private atStage(Stages.thirdRelease) {
+          require(now > thirdRelease);
+          uint256 amountToTransfer = balance / 2;
+          ERC20Token.transfer(beneficiary, amountToTransfer); // send 25 % of team releases
+          nextStage();
+      }
+      function fourth_release(uint256 balance) private atStage(Stages.fourthRelease) {
+          require(now > fourthRelease);
+          ERC20Token.transfer(beneficiary, balance); // send remaining 25 % of team releases
+      }
+
    }
-
-    function updateToken(address tokenAddressInput) external {
-       require(msg.sender == beneficiary);
-       ERC20Token = Token(tokenAddressInput);
-    }
-
-   function changeBeneficiary(address newBeneficiary) external {
-     require(msg.sender == beneficiary);
-     beneficiary = newBeneficiary;
-   }
-
-   function checkBalance() constant returns (uint256 tokenBalance){
-     return ERC20Token.balanceOf(this);
-   }
-
-   // in total 15% of C20 tokens will be sent to this contract
-
-   // EXPENSE ALLOCATION: 7.5%                      | TEAM ALLOCATION: 7.5% (vest over 2 years)
-   //   1% - ICO bonus                              | initalPayment: 1.5%
-   //   3% - Marketing (2.5% 8760, 0.5% BLOCK512)   | firstRelease: 1.5%
-   //   1% - Security (BLOCK512)                    | secondRelease: 1.5%
-   //   2% - legal                                  | thirdRelease: 1.5%
-   //   0.5% - Advisors                             | fourthRelease: 1.5%
-   // initial claim is tot expenses + initial team payment
-   // initial claim is thus (7.5 + 1.5)/15 = 60% of C20 tokens sent here
-   // each other release (for team) is 10% of tokens sent here
-
-   function claim() external {
-     require(msg.sender == beneficiary);
-     require(block.number > fundingEndBlock);
-
-     uint256 balance = ERC20Token.balanceOf(this);
-     uint256 amountToTransfer;
-
-     if(!initClaim){
-        firstRelease = now + 26 weeks; // assign 4 claiming times
-        secondRelease = firstRelease + 26 weeks;
-        thirdRelease = secondRelease + 26 weeks;
-        fourthRelease = thirdRelease + 26 weeks;
-
-        amountToTransfer = safeMul(balance, 60) / 100;
-        ERC20Token.transfer(beneficiary, amountToTransfer); // 40% tokens left
-        initClaim = true;
-     } else if(now > firstRelease && !firstDone){
-       amountToTransfer = balance / 4;
-       ERC20Token.transfer(beneficiary, amountToTransfer); // 30% tokens left
-       firstDone = true;
-     } else if(now > secondRelease && !secondDone){
-       amountToTransfer = balance / 3;
-       ERC20Token.transfer(beneficiary, amountToTransfer); // 20% tokens left
-       secondDone = true;
-     } else if(now > thirdRelease && !thirdDone){
-       amountToTransfer = balance / 2;
-       ERC20Token.transfer(beneficiary, amountToTransfer); // 10% tokens left
-       thirdDone = true;
-     } else if(now > fourthRelease){ // allow transfer of all remaining
-       ERC20Token.transfer(beneficiary, balance);
-     } else {
-       require(false); //todo: is this preferable to throw?
-     }
-
-   }
-
- }
